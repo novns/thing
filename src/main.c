c9ab8b81324589cdbf0413a92714a83c91796ef6
Nick Novikov <nick.novikov@gmail.com>
2021-12-24 00:22:19 +0800

    main: Logging tests

--- src/main.c
+++ src/main.c
@@ -1,5 +1,18 @@
 #include "main.h"
 
+
 int main()
 {
+    log_info("Starting %s", PACKAGE_STRING);
+
+#define exit(...)
+    log_fatal("Testing fatal message, exit() is disabled");
+#undef exit
+
+    log_error("Testing error message");
+    log_info("Testing info message");
+    log_test("Testing test message");
+    log_debug("Testing debug message");
+
+    return EXIT_SUCCESS;
 }
