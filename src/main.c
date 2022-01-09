7a62c9af89009c55df64e5e60ea84ec7c49e5b7a
Nick Novikov <nick.novikov@gmail.com>
2022-01-09 12:57:43 +0800

    main: Test hex dump

--- src/main.c
+++ src/main.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#include <time.h>
+
 
 int main()
 {
@@ -26,6 +28,16 @@ int main()
     log_info("Testing info message");
     log_test("Testing test message");
     log_debug("Testing debug message");
+
+#ifdef ENABLE_DEBUG
+    srand(time(NULL));
+
+    unsigned char dump_test[128];
+    for (int i = 0; i < 128; i++)
+        dump_test[i] = rand() % 256;
+
+    log_dump(dump_test, 128);
+#endif
 
 
     log_close();
